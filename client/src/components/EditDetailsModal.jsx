f162e70a579bae66926e24f09365aa9f3fbc3e23
Sharkz Reigns <87173929+SHARKZTECH@users.noreply.github.com>
2022-09-26 18:52:27 +0300

    1

--- client/src/components/EditDetailsModal.jsx
+++ client/src/components/EditDetailsModal.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 import { Col, Row,Form,Card,Button, Modal } from 'react-bootstrap';
-import { propTypes } from 'react-bootstrap/esm/Image';
+import FileBase from 'react-file-base64';
 
 const EditDetailsModal = (props) => {
+    const [userData,setUserData]=useState({
+        profilePic:"",
+        coverPic:"",
+    })
     const handleUpdate=(e)=>{
         e.preventDefault();
         props.onHide();
@@ -20,37 +25,50 @@ const EditDetailsModal = (props) => {
                 <Row>
                     <Col>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>First name</Form.Label>
                         <Form.Control type="text" placeholder="First name" />     
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Last name</Form.Label>
                         <Form.Control type="text" placeholder="Last name" />     
                         </Form.Group>
                     </Col>
                 </Row>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />     
+                    <Form.Control type="text" placeholder="WorksAt" />     
                     </Form.Group>
 
                 <Row>
                 <Col>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="text" placeholder="About" />
                         </Form.Group> 
                     </Col>
                     <Col>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control type="password" placeholder="Confirm Password" />
+                            <Form.Control type="text" placeholder="livesin" />
                         </Form.Group> 
                     </Col>
                 </Row>   
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Profile picture</Form.Label>
+                        <div className="file_input-container">
+                        <FileBase  multiple={false} onDone={({ base64 }) => setUserData({ ...userData, profilePic: base64 })} value={userData.profilePic}/>   
+                        </div> 
+                    </Form.Group> 
+                    </Col>
+                    <Col>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Cover picture</Form.Label>
+                        <div className="file_input-container">
+                        <FileBase  type="image" multiple={false} onDone={({ base64 }) => setUserData({ ...userData, coverPic: base64 })} value={userData.coverPic}/>   
+                        </div> 
+                    </Form.Group> 
+                    </Col>
+                </Row>  
                     <Button variant="primary" type="submit" onClick={handleUpdate}>
                     Update
                 </Button>
